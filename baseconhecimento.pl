f5e0fd9bda6a1951d4b19616eecf7b5ab971b0a4
FranciscoForte16 <francisco.forte1605@gmail.com>
2022-03-15 12:00:39 +0000

    update 2

--- baseconhecimento.pl
+++ baseconhecimento.pl
@@ -1,1 +1,17 @@
-if estadia_longa and zero_cem_euros and mais_de_dois_adultos and nenhuma_crianca and tem_animal and dois_quartos and duas_camas then casa1.
+if zero_cem_euros and nenhuma_pessoa and um_quarto then casa1.
+if zero_cem_euros and nenhuma_pessoa and dois_quartos then casa2.
+if zero_cem_euros and nenhuma_pessoa and tres_quartos then casa3.
+if zero_cem_euros and nenhuma_pessoa and quatro_quartos then casa4.
+
+if zero_cem_euros and uma_pessoa and um_quarto then casa1.
+if zero_cem_euros and uma_pessoa and dois_quartos then casa1.
+if zero_cem_euros and uma_pessoa and tres_quartos then casa1.
+if zero_cem_euros and uma_pessoa and quatro_quartos then casa1.
+
+if zero_cem_euros and duas_pessoas and quatro_quartos then casa1.
+if zero_cem_euros and duas_pessoas and quatro_quartos then casa1.
+if zero_cem_euros and duas_pessoas and quatro_quartos then casa1.
+if zero_cem_euros and duas_pessoas and quatro_quartos then casa1.
+
+if zero_cem_euros and nenhuma_pessoa and quatro_quartos then casa1.
+if zero_cem_euros and nenhuma_pessoa and quatro_quartos then casa1.
